ac27c16b6051885ece54ef5d3d768d71d92076f1
Hajime Morrita <omo@dodgson.org>
2015-08-19 10:23:00 -0700

    Do a remote access.

--- electron/src/hello.tsx
+++ electron/src/hello.tsx
@@ -1,8 +1,20 @@
+/// <reference path="../typings/github-electron/github-electron.d.ts" />
 /// <reference path="../typings/react/react.d.ts" />
 import React = require("react");
 
+var remote = require('remote');
+var remoteWindow : GitHubElectron.BrowserWindow = remote.getCurrentWindow();
+
 export class Hello extends React.Component<any, any> {
   render() : React.ReactElement<any> {
-    return <div>Hello From React!</div>;
+    return <div>
+      <h1>Hello From React!</h1>
+      <button onClick={this.handleFullscreen}>Fullscreen</button>
+    </div>;
+  }
+
+  handleFullscreen() {
+    console.log("Hello:" + remoteWindow.isFullScreen());
+    remoteWindow.setFullScreen(true);
   }
 };
